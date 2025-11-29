2eaf6cdf1e2652455fb805c43f3f7194574fc0a3
Leccon1 <dlavania703@gamil.com>
2025-11-29 22:48:20 +0300

    feat: add Header component with styles

---
+++ src/components/layuot/Header/Header.jsx
@@ -0,0 +1,15 @@
+import Logo from '../../common/Logo'
+import Navigation from '../Navigation/Navigation'
+
+import styles from './Header.module.scss'
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Logo />
+      <Navigation />
+    </header>
+  )
+}
+
+export default Header
