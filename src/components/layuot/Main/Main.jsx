0742702008464723acbfa1699df8b93ea771db08
Leccon1 <dlavania703@gamil.com>
2025-11-29 23:29:24 +0300

    feat: add Main component with styles

---
+++ src/components/layuot/Main/Main.jsx
@@ -0,0 +1,15 @@
+import styles from './Main.module.scss'
+
+const Main = () => {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.mainTitle}>Creative</h1>
+      <p className={styles.mainDecription}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eismod`
+      </p>
+      {/* <Button /> */}
+    </main>
+  )
+}
+
+export default Main
