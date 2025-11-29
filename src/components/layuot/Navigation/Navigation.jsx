9e1bb45da0c27df42f12bf447295d3bf288f8c41
Leccon1 <dlavania703@gamil.com>
2025-11-30 02:39:28 +0300

    feat: add nav Highlight with styles. Add const array to create links through the map

--- src/components/layuot/Navigation/Navigation.jsx
+++ src/components/layuot/Navigation/Navigation.jsx
@@ -1,22 +1,37 @@
+import { useState, useRef } from 'react'
+
 import styles from './Navigation.module.scss'
 
 const Navigation = ({ isActive, onClose }) => {
+  const [activeIndex, setActiveIndex] = useState(0)
+  const navItems = ['Home', 'About us', 'Gallery', 'Contact us', 'Search']
+  const navRef = useRef(null)
+
+  const handleLinkClick = (index) => {
+    setActiveIndex(index)
+  }
+
   return (
     <>
-      <div
-        className={`${styles.navOverlay} ${isActive ? `${styles.active}` : ''}`}
-        onClick={onClose}
-      />
-      <nav className={`${styles.nav} ${isActive ? `${styles.active}` : ''}`}>
+      <div className={`${styles.navOverlay} ${isActive ? styles.active : ''}`} onClick={onClose} />
+
+      <nav ref={navRef} className={`${styles.nav} ${isActive ? styles.active : ''}`}>
         <ul className={styles.navList}>
-          <li className={styles.navListItem}>Home</li>
-          <li className={styles.navListItem}>About us</li>
-          <li className={styles.navListItem}>Gallery</li>
-          <li className={styles.navListItem}>Contact us</li>
-          <li className={styles.navListItem}>Search</li>
+          {navItems.map((item, index) => (
+            <li key={item} className={styles.navListItem}>
+              <a
+                className={`${styles.navListLink} ${index === activeIndex ? styles.active : ''}`}
+                onClick={() => handleLinkClick(index)}
+              >
+                {item}
+              </a>
+            </li>
+          ))}
+          <div className={styles.navHighlight} />
         </ul>
       </nav>
     </>
   )
 }
+
 export default Navigation
