4cfb6b8f29ddfa651add8a136cd30ba0ad51b65d
Leccon1 <dlavania703@gamil.com>
2025-11-30 23:36:19 +0300

    feat: add button change themes with styles

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
+
 import styles from './Button.module.scss'
 
 const Button = () => {
-  return <button className={styles.button}>Change Themes</button>
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme)
+
+    // Меняем тему на всем документе
+    if (!isDarkTheme) {
+      document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+      document.documentElement.removeAttribute('data-theme')
+    }
+  }
+
+  return (
+    <button className={styles.button} onClick={toggleTheme}>
+      {isDarkTheme ? 'Light Theme' : 'Dark Theme'}
+    </button>
+  )
 }
 
 export default Button
