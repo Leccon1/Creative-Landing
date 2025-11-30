9b8b3a192e619cd227ab9d332aa13b16513542ab
Leccon1 <dlavania703@gamil.com>
2025-11-30 21:37:25 +0300

    feat: Added a check to remove the logo component if the screen width is greater than 768 pixels.

--- src/components/layuot/Navigation/Navigation.jsx
+++ src/components/layuot/Navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import Logo from '@components/common/Logo/Logo'
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 import styles from './Navigation.module.scss'
 
@@ -7,6 +7,18 @@ const Navigation = ({ isActive, onClose }) => {
   const [activeIndex, setActiveIndex] = useState(0)
   const navItems = ['Home', 'About us', 'Gallery', 'Contact us', 'Search']
   const navRef = useRef(null)
+  const [windowWidth, setWindowWidth] = useState(
+    typeof window !== 'undefined' ? window.innerWidth : 0
+  )
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth)
+    }
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   const handleLinkClick = (index) => {
     setActiveIndex(index)
@@ -16,8 +28,7 @@ const Navigation = ({ isActive, onClose }) => {
     <>
       <div className={`${styles.navOverlay} ${isActive ? styles.active : ''}`} onClick={onClose} />
       <nav ref={navRef} className={`${styles.nav} ${isActive ? styles.active : ''}`}>
-        <Logo />
-
+        {windowWidth < 768 && <Logo />}
         <ul className={styles.navList}>
           {navItems.map((item, index) => (
             <li key={item} className={styles.navListItem}>
